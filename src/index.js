9174ffbab9b69f39b49e7bef77b5404ceb002be8
Dhiraj <dhiraj@zazz.io>
2024-12-13 16:08:59 +0530

    Utils codes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
-const app = express();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error in connecting to DB", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+// export const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.error(error.code || 500, {
+//       success: false,
+//       message: error.message || "Something went wrong",
+//     });
+//   }
+// };
+
+export const asyncHandler = (fn) => {
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((error) => {
+      next(error);
+    });
+  };
+};
